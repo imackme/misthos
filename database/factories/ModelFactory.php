dad5b9bc57a11023705c547f5490710e05860223
Mark <bluemagician_201989@yahoo.com>
2017-02-18 17:49:13 +0800

    Initial repository

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'first_name' => $faker->firstName,
+        'middle_name' => $faker->lastName,
+        'last_name' => $faker->lastName,
+        'username' => $faker->unique()->userName,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
